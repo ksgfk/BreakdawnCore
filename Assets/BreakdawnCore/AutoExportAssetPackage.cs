bf565c4f6cd7c8499dd8e00dbe866f887bc8c46f
ksgfk <37179748+ksgfk@users.noreply.github.com>
2019-04-03 17:07:50 +0800

    自动导出unitypackage格式库文件

---
+++ Assets/BreakdawnCore/AutoExportAssetPackage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+using UnityEditor;
+
+namespace Breakdawn
+{
+	public class AutoExportAssetPackage
+	{
+		[MenuItem("Breakdawn/导出Asset Package %e")]
+		private static void DoExport()
+		{
+			string[] assetPathName = {"Assets/BreakdawnCore"};
+			var rootPath = $"{Path.Combine(Application.dataPath, "../")}";
+			var fileName = $"BreakdawnCore{DateTime.Now:yyyyMMdd_HHmmss}.unitypackage";
+
+			AssetDatabase.ExportPackage(assetPathName, fileName, ExportPackageOptions.Recurse);
+
+			var expFile = new FileInfo($"{rootPath}{fileName}");
+			if (!Directory.Exists($"{rootPath}/Build"))
+			{
+				Directory.CreateDirectory($"{rootPath}/Build");
+			}
+			expFile.MoveTo($"{rootPath}/Build/{fileName}");
+
+			Application.OpenURL($"file://{rootPath}/Build");
+		}
+	}
+}
