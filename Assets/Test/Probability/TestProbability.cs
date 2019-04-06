80bca0c55c6f71f239845fcb925c09e4614cb765
ksgfk <37179748+ksgfk@users.noreply.github.com>
2019-04-06 21:02:09 +0800

    测试概率方法
    （搞个精度真是闲得蛋疼= =

---
+++ Assets/Test/Probability/TestProbability.cs
@@ -0,0 +1,29 @@
+﻿using Breakdawn.Utils;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Breakdawn
+{
+	public class TestProbability : MonoBehaviour
+	{
+		private void Start()
+		{
+			List<bool> yes = new List<bool>();
+			List<bool> no = new List<bool>();
+			for (int a = 0; a < 1000000; a++)
+			{
+				if (Probability.Percent(51.25141F))
+				{
+					yes.Add(true);
+				}
+				else
+				{
+					no.Add(false);
+				}
+			}
+			Debug.Log($"{yes.Count}/1000000");
+			Debug.Log($"{no.Count}/1000000");
+		}
+	}
+}
