67cae24ab9718eae0397185fc9738a6ee1140815
ksgfk <37179748+ksgfk@users.noreply.github.com>
2019-04-19 23:33:03 +0800

    面向组件编程
    能力系统，只做了两个接口

---
+++ Assets/BreakdawnCore/Capability/ICapability.cs
@@ -0,0 +1,13 @@
+﻿namespace Breakdawn.Core
+{
+	/// <summary>
+	/// 能力
+	/// </summary>
+	/// <typeparam name="T">实现能力的类</typeparam>
+	public interface ICapability<T> where T : class
+	{
+		string CapabilityName { get; }
+
+		T GetCapability { get; }
+	}
+}
---
+++ Assets/BreakdawnCore/Capability/ICapabilityProvider.cs
@@ -0,0 +1,12 @@
+﻿namespace Breakdawn.Core
+{
+	/// <summary>
+	/// 能力提供者,有能力组件的类实现该接口
+	/// </summary>
+	public interface ICapabilityProvider
+	{
+		T GetCapability<T>() where T : class, ICapability<T>;
+
+		void SetCustomCapability<T>(T capability) where T : class, ICapability<T>;
+	}
+}
