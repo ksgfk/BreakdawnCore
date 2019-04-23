f1d84b73dbeb93bc8846bb88ffc0ceed0856bd57
ksgfk <37179748+ksgfk@users.noreply.github.com>
2019-04-23 13:41:53 +0800

    没啥东西

---
+++ Assets/BreakdawnCore/Utils/ResourceLoader.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Breakdawn.Core
+{
+	/// <summary>
+	/// TODO：自定义资源，以后再学
+	/// </summary>
+	[Singleton]
+	//[CreateAssetMenu(fileName = "Bullet", menuName = "New Bullet", order = 1)]
+	public class ResourceLoader : ScriptableObject
+	{
+		private ResourceLoader() { }
+	}
+}
