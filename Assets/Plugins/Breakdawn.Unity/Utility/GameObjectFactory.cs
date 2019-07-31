d5b9395a0fbb1909d7982d47d5247fcce79d0941
ksgfk <37179748+ksgfk@users.noreply.github.com>
2019-07-31 22:08:10 +0800

    feat:GameObject对象池

---
+++ Assets/Plugins/Breakdawn.Unity/Utility/GameObjectFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Breakdawn.Core;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Breakdawn.Unity
+{
+    public class GameObjectFactory : IFactory<GameObject>
+    {
+        private readonly GameObject _prefab;
+
+        public GameObjectFactory(GameObject prefab)
+        {
+            _prefab = prefab ? prefab : throw new ArgumentNullException(nameof(prefab));
+        }
+
+        public GameObject Get()
+        {
+            return Object.Instantiate(_prefab);
+        }
+    }
+}
---
+++ Assets/Plugins/Breakdawn.Unity/Utility/GameObjectPool.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Breakdawn.Core;
+using UnityEngine;
+
+namespace Breakdawn
+{
+    public class GameObjectPool : ObjectPool<GameObject>
+    {
+        public GameObjectPool(IFactory<GameObject> factory, int initCount = 0) : base(factory, initCount)
+        {
+        }
+
+        public GameObjectPool(Func<GameObject> func, int initCount = 0) : base(func, initCount)
+        {
+        }
+    }
+}
