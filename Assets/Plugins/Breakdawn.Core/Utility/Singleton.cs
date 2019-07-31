6b26c58ceccb57218369dee37fe036d60c2fd4e3
ksgfk <37179748+ksgfk@users.noreply.github.com>
2019-07-31 22:35:02 +0800

    feat:单例模式

---
+++ Assets/Plugins/Breakdawn.Core/Utility/Singleton.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Breakdawn.Core
+{
+    public abstract class Singleton<T> where T : class
+    {
+        private static readonly Lazy<T> InstanceConstructor = new Lazy<T>(() =>
+        {
+            var type = typeof(T);
+            if (type.IsAbstract)
+            {
+                throw new ArgumentException($"单例类{type.FullName}不能是抽象的");
+            }
+
+            var constructors =
+                type.GetConstructors(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+            if (constructors.Length != 1)
+            {
+                throw new ArgumentException($"单例类{type.FullName}不能有多个构造方法");
+            }
+
+            var constructor = constructors.SingleOrDefault(c => !c.GetParameters().Any() && c.IsPrivate);
+            if (constructor == null)
+            {
+                throw new ArgumentException($"单例类{type.FullName}的构造方法必须是私有且无参的");
+            }
+
+            return Activator.CreateInstance(type, true) as T;
+        }, true);
+
+        public static T Instance => InstanceConstructor.Value;
+    }
+}
---
+++ Assets/Plugins/Breakdawn.Unity/Utility/MonoSingleton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+namespace Breakdawn.Unity
+{
+    public class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        public static T Instance { get; private set; }
+
+        protected void InitInstance()
+        {
+            if (Instance != null)
+            {
+                Debug.LogWarning($"单例类{typeof(T).FullName}已经初始化过了,不需要重复初始化");
+                return;
+            }
+
+            if (this is T singleton)
+            {
+                Instance = singleton;
+            }
+            else
+            {
+                throw new ArgumentException($"单例类{typeof(T).FullName}类型错误");
+            }
+        }
+
+        protected void DisposeInstance()
+        {
+            Instance = null;
+        }
+    }
+}
