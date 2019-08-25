04dfabbea085f22883494502022b9637d4667148
ksgfk <37179748+ksgfk@users.noreply.github.com>
2019-08-25 22:47:08 +0800

    wip:导出AB包

---
+++ Assets/Plugins/Breakdawn.Unity/Editor/ExportAssetBundle.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+
+namespace Breakdawn.Unity.Editor
+{
+    public class ExportAssetBundle
+    {
+        [MenuItem("Breakdawn/构建Asset Bundle")]
+        public static void Build()
+        {
+            var abConfig =
+                AssetDatabase.LoadAssetAtPath<ExportAssetBundleConfig>("Assets/Resources/ExportABConfig.asset");
+        }
+    }
+}
---
+++ Assets/Plugins/Breakdawn.Unity/Editor/ExportAssetBundleConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace Breakdawn.Unity.Editor
+{
+    [CreateAssetMenu(fileName = "ExportABConfig", menuName = "CreateExportABConfig", order = 0)]
+    public class ExportAssetBundleConfig : ScriptableObject
+    {
+        public List<string> allPrefabPaths = new List<string>();
+        public List<ABFileDirName> allABFileDirName = new List<ABFileDirName>();
+    }
+
+    [Serializable]
+    public struct ABFileDirName
+    {
+        public string Name;
+        public string Path;
+    }
+}
